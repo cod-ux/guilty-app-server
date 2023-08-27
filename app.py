12c8c43370f7e176a1a9cd0a854ba8da6da4c6b9
Surya Govindasamy Ganesan <suryaganesan925@gmail.com>
2023-08-27 10:18:54 +0100

    adding credentials.json and editing app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-import math
+#Write code to intialize firebase from credentials.json
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate('credentials.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+users_ref = db.collection('users')
+
+#write code to get list of all docs from firestore
+docs = users_ref.stream()
+
+# write code to read data from a particular doc
+doc_ref = db.collection('users').document('42xGhEiG9Fe0YZ2DAOBoFHTDEYF2')
+
+print(doc_ref.get().to_dict())
