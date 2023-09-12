0f3709682487dd983211ee6d5eb692b5cc3308c1
Surya Govindasamy Ganesan <suryaganesan925@gmail.com>
2023-09-12 20:03:31 +0100

    added link

--- pld.py
+++ pld.py
@@ -19,14 +19,8 @@ from plaid.model.transactions_refresh_request import TransactionsRefreshRequest
 from plaid.model.accounts_balance_get_request import AccountsBalanceGetRequest
 
 
-public_token = 'public-development-98f5f9a7-e038-4e79-8f2d-046a545c173e'
+#public_token = 'public-development-98f5f9a7-e038-4e79-8f2d-046a545c173e'
 #access_token = "access-development-bfe6171e-82dc-44cb-a02e-780b738d17b1" 
-
-# fields to populate: account_balance, tab, day_add
-# additional fields used: monthly_budget, start_date
-# output: day_balance, runway
-
-
 
 def get_real_balance(access_token):
     configuration = plaid.Configuration(
@@ -45,3 +39,56 @@ def get_real_balance(access_token):
     accounts = response['accounts']
     return accounts[-1].to_dict()['balances']['available']
 
+
+
+
+def request_link(): #previously username was 'test_user'
+        client_id = '6377de812443a60012a4c1db'
+        secret = 'e978853789ea98468172615b7a807e'
+
+        configuration = plaid.Configuration(
+        host = plaid.Environment.Development,
+        api_key = {
+            'clientId': client_id,
+            'secret': secret,
+        }
+        )
+
+        api_config = plaid.ApiClient(configuration)
+        client = Connect.PlaidApi(api_config)
+        request = Connect.LinkTokenCreateRequest(
+               products=[Products('auth'), Products('transactions')],
+               client_name='Guilty Corporations',
+               country_codes=[CountryCode('GB')],
+               language = 'en',
+               user = LinkTokenCreateRequestUser(
+                     client_user_id = client_id
+               )
+               )
+
+        response = client.link_token_create(request)
+        link_token = response['link_token']
+            
+        return link_token
+    
+def init_exchange_request(public_token):
+    client_id = '6377de812443a60012a4c1db'
+    secret = 'e978853789ea98468172615b7a807e'
+
+    configuration = plaid.Configuration(
+    host = plaid.Environment.Development,
+    api_key = {
+            'clientId': client_id,
+            'secret': secret,
+    }
+    )
+
+    api_config = plaid.ApiClient(configuration)
+    client = Connect.PlaidApi(api_config)
+    exchange_request = exc_request(
+       public_token = public_token
+    )
+
+    exchange_response = client.item_public_token_exchange(exchange_request)
+    access_token = exchange_response['access_token']
+    return access_token
